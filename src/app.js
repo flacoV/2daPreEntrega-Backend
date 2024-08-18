f4f413247afa12517a710a6c30be37af4045c129
devFlaco <npressure00@gmail.com>
2024-08-18 18:36:18 -0300

    build(admin panel for store)

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { __dirname } from './utils.js';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io'
+
+import routerP from './routes/products.router.js';
+import routerV from './routes/views.router.js';
+import socketProducts from './listeners/socketProducts.js';
+
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars');
+
+app.use('/api', routerP)
+app.use('/', routerV)
+
+const httpServer = app.listen(PORT, () => {
+    try {
+        console.log(`Server is running on port ${PORT}\n Acceder a:`);
+        console.log(`\t1). http://localhost:${PORT}/api/products`);
+        console.log(`\t2). http://localhost:${PORT}/api/carts`);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+const socketServer = new Server(httpServer)
+
+socketProducts(socketServer)
